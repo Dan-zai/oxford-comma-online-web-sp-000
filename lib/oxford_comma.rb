abd44ad8624bbf783cf1e30e54f0dfc09e858baf
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-01-27 04:18:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2 
   array.join(" and ")
   elsif array.length == 3
-  return 
+  return "#{array[0]}, #{array[1]}, #{array[2]} and #{array[3]}"
 end
 end
